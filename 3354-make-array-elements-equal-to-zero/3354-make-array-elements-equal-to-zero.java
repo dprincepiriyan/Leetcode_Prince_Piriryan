2df4242f68025e8488ba7e01c9f761148324e26e
D PRINCE PIRIYAN <princepiriyan07183@gmail.com>
2026-06-07 10:30:30 +0530

    Time: 0 ms (100%), Space: 43.9 MB (58.79%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
@@ -1,19 +1,24 @@
 class Solution {
     public int countValidSelections(int[] nums) {
-        int total = 0, ls = 0, cases = 0;
-        for (int num : nums) total += num;
-
-        for (int num : nums) {
-            int rs = total - ls - num;
-            if (num == 0) {
-                if (ls == rs) {
-                    cases += 2;
-                } else if (ls == rs - 1 || ls - 1 == rs) {
-                    cases += 1;
-                }
-            }
-            ls += num;
+        int total=0;
+        int n=nums.length;
+        for(int num:nums){
+            total+=num;
         }
-        return cases;
+        int count=0;
+        int leftsum=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+            int rightsum=total-leftsum-nums[i];
+            if(rightsum==leftsum){
+                count+=2;
+            }else if(Math.abs(rightsum-leftsum)==1){
+                count+=1;
+            }
+            
+        }
+        leftsum+=nums[i];
+        }
+        return count;
     }
 }
