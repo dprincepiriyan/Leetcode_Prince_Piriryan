7867c0c52347f5e7d467080fa0af150b131af017
D PRINCE PIRIYAN <princepiriyan07183@gmail.com>
2026-08-08 08:12:49 +0530

    Time: 2 ms (99.34%), Space: 47.2 MB (40.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,34 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] copy=new int[nums.length];
+        Map<Integer,Integer> map= new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            copy[i]=nums[i];
-        }
-        Arrays.sort(nums);
-        int left=0;
-        int right=nums.length-1;
-        int leftnum=0;
-        int rightnum=0;
-        while(left<right){
-            if(nums[left]+nums[right]==target){
-                leftnum=nums[left];
-                rightnum=nums[right];
-                break;
-            }else if(nums[left]+nums[right]>target){
-                right--;
-            }else{
-                left++;
+            int compliment=target-nums[i];
+            if(map.containsKey(compliment)){
+                return new int[]{map.get(compliment),i};
             }
+            map.put(nums[i],i);
         }
-        int a=-1;
-        int b=-1;
-        for(int i=0;i<nums.length;i++){
-            if(copy[i]==leftnum &&a==-1){
-                a=i;
-            }else if(copy[i]==rightnum && b==-1){
-                b=i;
-            }
-        }
-        return new int[]{a,b};
+        return new int[]{};
     }
 }
