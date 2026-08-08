945e5430a6dfa91e51be89338f22292c20422c6d
D PRINCE PIRIYAN <princepiriyan07183@gmail.com>
2026-08-08 08:07:08 +0530

    Time: 7 ms (47%), Space: 47.4 MB (11.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,34 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int a=0;
-        int b=0;
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    a=i;
-                    b=j;
-                }
+        int[] copy=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            copy[i]=nums[i];
+        }
+        Arrays.sort(nums);
+        int left=0;
+        int right=nums.length-1;
+        int leftnum=0;
+        int rightnum=0;
+        while(left<right){
+            if(nums[left]+nums[right]==target){
+                leftnum=nums[left];
+                rightnum=nums[right];
+                break;
+            }else if(nums[left]+nums[right]>target){
+                right--;
+            }else{
+                left++;
             }
         }
-        return new int[] {a,b};
+        int a=-1;
+        int b=-1;
+        for(int i=0;i<nums.length;i++){
+            if(copy[i]==leftnum &&a==-1){
+                a=i;
+            }else if(copy[i]==rightnum && b==-1){
+                b=i;
+            }
+        }
+        return new int[]{a,b};
     }
 }
